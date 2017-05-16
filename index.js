98a79a8cb3e2d4a4fe378005b2730ac6c399b0a5
calberca <cesalberca@gmail.com>
2017-05-16 18:15:15 +0200

    Removed promises for the moment

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const npmRun = require('npm-run');
 const path = require('path');
 const marked = require('marked');
-const fs = require('fs');
 const Promise = require('bluebird');
-Promise.promisifyAll(fs);
+const fs = Promise.promisifyAll(require('fs'));
 
 console.log('Processing posts')
 npmRun.exec('./node_modules/.bin/doctoc posts');
-
 console.log('Parsing to hmtl')
 
-async function init() {
-  const files = await fs.readdir('./posts');
-  files.forEach(file => {
-    const content = await fs.readFile(file);
-    console.log(content);
+function init() {
+  const files = fs.readdirSync('./posts');
+  console.log(files)
+  files.forEach((file) => {
+    const content = fs.readFileSync(file);
+    parseToHtml(content);
   });
 }
 
